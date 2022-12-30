a3530f06a381f8d2141c644fae2a7c18a4615181
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-12-29 21:04:05 -0300

    update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2130,7 +2130,12 @@ class PlayState extends MusicBeatState
 		songBanner.cameras = [camCustom];
 		songBannerText.cameras = [camCustom];
 		doof.cameras = [camHUD];
-		
+
+		#if android
+		addAndroidControls();
+		androidControls.visible = true;
+		#end
+
 		// if (SONG.song == 'South')
 		// FlxG.camera.alpha = 0.7;
 		// UI_camera.zoom = 1;
@@ -4436,7 +4441,7 @@ class PlayState extends MusicBeatState
 			botplayTxt.alpha = 1 - Math.sin((Math.PI * botplaySine) / 180);
 		}
 
-		if (controls.PAUSE && startedCountdown && canPause)
+		if (controls.PAUSE #if android || FlxG.android.justReleased.BACK #end && startedCountdown && canPause)
 		{
 			var ret:Dynamic = callOnLuas('onPause', []);
 			if(ret != FunkinLua.Function_Stop) {
