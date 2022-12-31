fb08c05ec44810019cde63a5b9ee4a6e2b0ee135
Kaique62 <81991014+Kaique62@users.noreply.github.com>
2022-12-30 22:11:47 -0300

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -225,10 +225,12 @@ class TitleState extends MusicBeatState
 			/*var diamond:FlxGraphic = FlxGraphic.fromClass(GraphicTransTileDiamond);
 			diamond.persist = true;
 			diamond.destroyOnNoUse = false;
+
 			FlxTransitionableState.defaultTransIn = new TransitionData(FADE, FlxColor.BLACK, 1, new FlxPoint(0, -1), {asset: diamond, width: 32, height: 32},
 				new FlxRect(-300, -300, FlxG.width * 1.8, FlxG.height * 1.8));
 			FlxTransitionableState.defaultTransOut = new TransitionData(FADE, FlxColor.BLACK, 0.7, new FlxPoint(0, 1),
 				{asset: diamond, width: 32, height: 32}, new FlxRect(-300, -300, FlxG.width * 1.8, FlxG.height * 1.8));
+
 			transIn = FlxTransitionableState.defaultTransIn;
 			transOut = FlxTransitionableState.defaultTransOut;*/
 
@@ -1011,10 +1013,13 @@ class TitleState extends MusicBeatState
 	{
 		if (!ClientPrefs.funiShaders || skippedIntro)
 			return;
+
 		if (T != null)
 			T.cancel();
+
 		if (chrom != null && setChrom)
 			chrom.setChrome(FlxG.random.float(0.0, 0.002));
+
 		new FlxTimer().start(FlxG.random.float(0.08, 0.12), function(tmr:FlxTimer)
 		{
 			new FlxTimer().start(FlxG.random.float(0.7, 1.6), function(tmr:FlxTimer)
